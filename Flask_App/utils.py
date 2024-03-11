406ee179f0f6426bcebdd8ca1235c0d46cb2893a
George <george.stander@gmail.com>
2024-03-11 20:40:03 +0200

    flask updates

---
+++ Flask_App/utils.py
@@ -0,0 +1,16 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+import os
+
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+
+
+
+def generate_ideas(prompt):
+  response = genai.generate_text(prompt, max_output_tokens=300)
+  ideas = textwrap.wrap(response.result, 60)
+  return ideas
