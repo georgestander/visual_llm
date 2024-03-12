76fc276e8859844203caf60c178dc8a2af689ee3
George <george.stander@gmail.com>
2024-03-12 21:23:36 +0200

    chat history

--- Flask_App/__init__.py
+++ Flask_App/__init__.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from utils import generate_ideas
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         prompt = request.form['prompt']
         ideas = generate_ideas(prompt)
-        return render_template('index.html', ideas=ideas)
-    return render_template('index.html')
+
+        # Store the chat history in the session
+        chat_history = session.get('chat_history', [])
+        chat_history.append({'role': 'user', 'content': prompt})
+        chat_history.extend([{'role': 'assistant', 'content': idea} for idea in ideas])
+        session['chat_history'] = chat_history
+
+        return render_template('index.html', ideas=ideas, chat_history=chat_history)
+    return render_template('index.html', chat_history=session.get('chat_history', []))
